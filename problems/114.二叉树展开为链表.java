39a345a2a48a7498ae8bcb3b1456824665c6c56d
sqzhang <opassion.thu@gmail.com>
2019-12-12 21:11:07 +0800

    20191212

---
+++ problems/114.二叉树展开为链表.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=java
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (66.11%)
+ * Likes:    215
+ * Dislikes: 0
+ * Total Accepted:    19.9K
+ * Total Submissions: 29.9K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给定一个二叉树，原地将它展开为链表。
+ * 
+ * 例如，给定二叉树
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \   \
+ * 3   4   6
+ * 
+ * 将其展开为：
+ * 
+ * 1
+ * ⁠\
+ * ⁠ 2
+ * ⁠  \
+ * ⁠   3
+ * ⁠    \
+ * ⁠     4
+ * ⁠      \
+ * ⁠       5
+ * ⁠        \
+ * ⁠         6
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        while(root != null) {
+            if (root.left == null) {
+                root = root.right;
+            } else {
+                TreeNode left = root.left;
+                while(left.right != null) {
+                    left = left.right;
+                }
+
+                left.right = root.right;
+                root.right = root.left;
+                root.left = null;
+                root = root.right;
+            }
+        }
+    }
+}
+// @lc code=end
+
