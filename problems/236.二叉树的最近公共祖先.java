154bc9730129ae2cedd8357c256ec180665e2ded
sqzhang <opassion.thu@gmail.com>
2019-12-16 21:09:39 +0800

    20191216

--- problems/236.二叉树的最近公共祖先.java
+++ problems/236.二叉树的最近公共祖先.java
@@ -61,7 +61,17 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        if (l != null && r != null) {
+            return root;
+        }
         
+        return l == null ? r : l;
     }
 }
 // @lc code=end
--- problems/301.删除无效的括号.java
+++ problems/301.删除无效的括号.java
@@ -39,7 +39,60 @@
 // @lc code=start
 class Solution {
     public List<String> removeInvalidParentheses(String s) {
-        
+        int left = 0, right = 0;
+        for(char c : s.toCharArray()) {
+            if (c == '(') {
+                left++;
+            } else if (c == ')') {
+                if (left > 0) {
+                    left--;
+                } else {
+                    right++;
+                }
+            }
+        }
+
+        List<String> res = new ArrayList<>();
+        dfs(s, left, right, new StringBuilder(s), 0, res);
+        return res;
+    }
+
+    private void dfs(String s, int l, int r, StringBuilder sb, int start, List<String> res) {
+        if (l == 0 && r == 0 && isValid(sb.toString())) {
+            res.add(sb.toString());
+            return;
+        }
+
+        for(int i = start;i < s.length();i++) {
+            if (i != start && s.charAt(i) == s.charAt(i - 1)) {
+                continue;
+            }
+            if (s.charAt(i) == '(' || s.charAt(i) == ')') {
+                sb = new StringBuilder(s);
+                sb.deleteCharAt(i);
+                if (r > 0 && s.charAt(i) == ')') {
+                    dfs(sb.toString(), l ,r - 1, sb, i, res);
+                } else if (l > 0 && s.charAt(i) == '(') {
+                    dfs(sb.toString(), l - 1, r, sb, i, res);
+                }
+            }
+        }
+    }
+
+    private boolean isValid(String s) {
+        int count = 0;
+        for(char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+            }
+            if (c == ')') {
+                count--;
+            }
+            if(count < 0) {
+                return false;
+            }
+        }
+        return count == 0;
     }
 }
 // @lc code=end
